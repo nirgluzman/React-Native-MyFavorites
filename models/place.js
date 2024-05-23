397e1498884f2d4e668cd610ebaa0287835e1e2a
nirgluzman <nir.gluzman@gmail.com>
2024-05-23 17:04:23 +0200

    define Place template object

---
+++ models/place.js
@@ -0,0 +1,10 @@
+class Place {
+  constructor(title, imageUri, address, location) {
+    this.title = title;
+    this.imageUri = imageUri;
+    this.address = address;
+    this.location = location; // { lat: 0.0, lng: 0.0 }
+    this.id = new Date().toString() + Math.random().toString();
+  }
+}
+export default Place;
