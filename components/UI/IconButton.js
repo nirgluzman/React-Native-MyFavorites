372754b73df7fa08bf743aed4b8054ff4ba4ffa7
nirgluzman <nir.gluzman@gmail.com>
2024-05-23 21:12:15 +0200

    add custom icon button

---
+++ components/UI/IconButton.js
@@ -0,0 +1,27 @@
+// custom icon button
+
+import { Pressable, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+export default function IconButton({ icon, size, color, onPress }) {
+  return (
+    <Pressable
+      style={({ pressed }) => [StyleSheet.button, pressed && styles.pressed]}
+      onPress={onPress}>
+      <Ionicons name={icon} size={size} color={color} />
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 8,
+    margin: 4,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  pressed: {
+    opacity: 0.7
+  }
+});
