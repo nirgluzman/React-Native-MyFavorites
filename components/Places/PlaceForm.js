6dcd8d93253c87e21d9a610bdc069a5d1f8ac80b
nirgluzman <nir.gluzman@gmail.com>
2024-05-24 00:05:04 +0200

    add title box, incl. styling

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,7 +1,45 @@
+import { useState } from 'react';
+import { ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+
+import { Colors } from '../../constants/colors'; // color palette
+
 export default function PlaceForm() {
+  const [enteredTitle, setEneteredTitle] = useState('');
+
+  function changeTitleHandler(enteredText) {
+    setEneteredTitle(enteredText);
+  }
+
   return (
-    <View>
-      <Text>The Place Form</Text>
-    </View>
+    <ScrollView style={styles.form}>
+      <View>
+        <Text style={styles.label}>Title</Text>
+        <TextInput style={styles.input} onChangeText={changeTitleHandler} value={enteredTitle} />
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1, // takes all the available space possible.
+    flexDirection: 'column', // default is 'column'.
+    padding: 24
+  },
+
+  label: {
+    fontWeight: 'bold',
+    marginBottom: 4,
+    color: Colors.primary500
+  },
+
+  input: {
+    marginVertical: 8,
+    paddingHorizontal: 4,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderBottomColor: Colors.primary700,
+    borderBottomWidth: 2,
+    backgroundColor: Colors.primary100
+  }
+});
