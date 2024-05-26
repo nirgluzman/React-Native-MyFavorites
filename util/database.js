cf9b127355876ef00e60c29e3a42b581782e9fdc
nirgluzman <nir.gluzman@gmail.com>
2024-05-26 22:24:41 +0200

    add helper functions to handle SQLite DB initialization

--- util/database.js
+++ util/database.js
@@ -1,12 +1,14 @@
-// helper functions to work with SQLite
+// helper functions to work with SQLite.
 
 import * as SQLite from 'expo-sqlite';
-import { Place } from '../models/place';
 
 // open a database.
-const db = await SQLite.openDatabaseAsync('places.db');
+export async function openDB() {
+  return await SQLite.openDatabaseAsync('places.db');
+}
 
-export async function initDB() {
+// initialize the database.
+export async function initDB(db) {
   return await db.execAsync(
     `CREATE TABLE IF NOT EXISTS places (
       id INTEGER PRIMARY KEY NOT NULL,
@@ -17,28 +19,4 @@ export async function initDB() {
       lng REAL NOT NULL
     )`
   );
-
-  // const promise = new Promise((resolve, reject) => {
-  //   db.transaction(tx => {
-  //     tx.executeSql(
-  //       `CREATE TABLE IF NOT EXISTS places (
-  //         id INTEGER PRIMARY KEY NOT NULL,
-  //         title TEXT NOT NULL,
-  //         imageUri TEXT NOT NULL,
-  //         address TEXT NOT NULL,
-  //         lat REAL NOT NULL,
-  //         lng REAL NOT NULL
-  //       )`,
-  //       [],
-  //       () => {
-  //         resolve();
-  //       },
-  //       (_, error) => {
-  //         reject(error);
-  //       }
-  //     );
-  //   });
-  // });
-
-  // return promise;
 }
