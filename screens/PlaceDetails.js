9268fc853855360cf3092c95186f86a9774a6a64
nirgluzman <nir.gluzman@gmail.com>
2024-05-27 00:14:33 +0200

    add screen template including styling

--- screens/PlaceDetails.js
+++ screens/PlaceDetails.js
@@ -1,3 +1,55 @@
-export default function PlaceDetails() {
-  return <></>;
+import { useEffect } from 'react';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+
+import OutlinedButton from '../components/UI/OutlinedButton';
+
+import { Colors } from '../constants/colors'; // color palette
+
+export default function PlaceDetails({ route }) {
+  function showOnMapHandler() {}
+
+  // fetch place details for a specific placeId when the screen loads.
+  const selectedPlaceId = route.params.placeId;
+
+  useEffect(() => {
+    // use selectedPlaceId to fetch data from SQLite for a single place.
+  }, [selectedPlaceId]);
+
+  return (
+    <ScrollView>
+      <Image style={styles.image} />
+      <View style={styles.locationContainer}>
+        <View style={styles.addressContainer}>
+          <Text style={styles.address}></Text>
+        </View>
+        <OutlinedButton icon='map' onPress={showOnMapHandler}>
+          View on Map
+        </OutlinedButton>
+      </View>
+    </ScrollView>
+  );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    height: '35%',
+    minHeight: 300,
+    width: '100%'
+  },
+
+  locationContainer: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  addressContainer: {
+    padding: 20
+  },
+
+  address: {
+    color: Colors.primary500,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 16
+  }
+});
