c3a9b158e25776579d43f4200aa3e09aebf89116
nirgluzman <nir.gluzman@gmail.com>
2024-05-26 09:19:42 +0200

    extract the new place info from route params and add that to places state

--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,5 +1,20 @@
+import { useEffect, useState } from 'react';
+import { useIsFocused } from '@react-navigation/native';
+
 import PlacesList from '../components/Places/PlacesList.js';
 
-export default function AllPlaces() {
-  return <PlacesList />;
+export default function AllPlaces({ route }) {
+  const [loadedPlaces, setLoadedPlaces] = useState([]);
+
+  const isFocused = useIsFocused(); // check if the screen is focused
+
+  useEffect(() => {
+    if (isFocused && route.params) {
+      // check if the screen is focused and there are params
+      const { place } = route.params;
+      setLoadedPlaces(currentPlaces => [place, ...currentPlaces]);
+    }
+  }, [isFocused, route]);
+
+  return <PlacesList places={loadedPlaces} />;
 }
